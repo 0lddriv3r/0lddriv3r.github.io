c1551eea4ff4f3a023a8fdc20f806f0aa329d71c
JiajieZhuo <jiajiezhuo@163.com>
2019-06-11 22:17:44 +0800

    Put onload js in resume.html.

--- js/language.js
+++ js/language.js
@@ -1,28 +1,4 @@
-$(function(){ 
-    var script=document.createElement("script");  
-    script.type="text/javascript";  
-    script.src="https://www.microsoftTranslator.com/ajax/v3/WidgetV3.ashx?siteData=ueOIGRSKkd965FeEGM5JtQ**";  
-    document.getElementsByTagName('head')[0].appendChild(script);  
 
-
-    var value = sessionStorage.getItem("language");
-    document.onreadystatechange = function () {
-        if (document.readyState == 'complete') {
-            if(value==="1"){
-                Microsoft.Translator.Widget.Translate('zh-CHS', 'en', onProgress, onError, onComplete, onRestoreOriginal, 2000);
-            }
-        }
-    }
-    function onProgress(value) {
-    }
-    function onError(error) {
-    }
-    function onComplete() {
-        $("#WidgetFloaterPanels").hide();
-    }
-    function onRestoreOriginal() { 
-    }
-});
 
 function translate(){
     var value = sessionStorage.getItem("language");
